d2aed7c7552d262042f61dbd48d430446679935e
OksanaPl <ok965@yandex.ru>
2023-01-27 18:31:13 +0300

    1.1. Создание объекта [#165452 #385402]

--- src/main/java/ru/job4j/oop/Dog.java
+++ src/main/java/ru/job4j/oop/Dog.java
@@ -4,6 +4,6 @@ public class Dog {
     public static void main(String[] args) {
         Dog polkan = new Dog();
         Dog sharik = new Dog();
-        Dog Zhychka = new Dog();
+        Dog zhychka = new Dog();
     }
 }
