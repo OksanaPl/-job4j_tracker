d180aa27ff92c5c0f347384444ad728622b8b163
OksanaPl <ok965@yandex.ru>
2023-02-13 22:35:29 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,46 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        int arraySize = items.length;
+        Item[] names = new Item[arraySize];
+        int itemsSize = 0;
+        for (int i = 0; i < arraySize; i++) {
+            Item name = items[i];
+            if (name != null) {
+                names[itemsSize] = name;
+                itemsSize++;
+            }
+        }
+
+        Item[] newNames = Arrays.copyOf(names, itemsSize);
+        for (int i = 0; i < itemsSize; i++) {
+            System.out.println(newNames[i].getName());
+        }
+
+        return newNames;
+    }
+
+    public Item[] findByName(String key) {
+        int arraySize = items.length;
+        Item[] names = new Item[arraySize];
+        int itemsSize = 0;
+        for (int i = 0; i < arraySize; i++) {
+            Item name = items[i];
+            if (name != null && name.getName().equals(key)) {
+                names[itemsSize] = name;
+                itemsSize++;
+            }
+        }
+
+        Item[] newNames = Arrays.copyOf(names, itemsSize);
+        for (int i = 0; i < itemsSize; i++) {
+            System.out.println(newNames[i].getName());
+        }
+
+        return newNames;
+
     }
 }
