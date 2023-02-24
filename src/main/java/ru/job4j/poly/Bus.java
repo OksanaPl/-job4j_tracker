9291fdaa39441e92dac99534a0e0376e0f63c96c
OksanaPl <ok965@yandex.ru>
2023-02-24 22:15:49 +0300

    4.2. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,19 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    @Override
+    public void drive() {
+        System.out.println("Максимально разрешенная скорость 90 км/ч");
+    }
+
+    @Override
+    public void passengers(int quantity) {
+        System.out.println(quantity);
+    }
+
+    @Override
+    public double refuel(int fuel) {
+        double literPrice = 52;
+        return fuel * literPrice;
+    }
+}
