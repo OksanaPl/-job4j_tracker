c0ddd608c895387c757b0ac817511f333995458c
OksanaPl <ok965@yandex.ru>
2023-03-29 21:48:29 +0300

    2. Идентичные тексты [#212655]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,23 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = false;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> checkOrigin = new HashSet<>();
+        HashSet<String> checkText = new HashSet<>();
+        for (String s : origin) {
+            checkOrigin.add(s);
+        }
+        for (String s : text) {
+            checkText.add(s);
+        }
+        if (checkOrigin.containsAll(checkText)) {
+            rsl = true;
+        }
+        return rsl;
+    }
+}
